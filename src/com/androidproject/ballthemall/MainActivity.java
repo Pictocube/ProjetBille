06cbfae42e109cab92aad0e2f56de6247f5ce6a3
Pictocube <kevin.ligonesche@gmail.com>
2014-04-04 12:25:36 +0200

    gestion du bg center

--- src/com/androidproject/ballthemall/MainActivity.java
+++ src/com/androidproject/ballthemall/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-
+ 
 public class MainActivity extends Activity {
 	
 	private Button start;  // declaration bouton start
